3e24fe5a21582dc45482e00f450c24ec7db20d81
Chester Cheung <cheung319@gmail.com>
2023-02-27 21:53:47 -0500

    solution

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-  // type your code here
+  if (num < 2) {
+    return num;
+  }
+
+  let lastTwo = [0, 1];
+
+  for (let i = 0; i < num - 1; ++i) {
+    const sum = lastTwo[0] + lastTwo[1];
+    lastTwo = [lastTwo[1], sum];
+  }
+
+  return lastTwo[1];
 }
 
 if (require.main === module) {
@@ -20,5 +31,6 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// Takes an argument
+// For loop, with i = o , i < argument, i++
+// take arugment length - 1 and argument legnth -2 and add
